4960310a037148ae34b3f8212c4d5f5fe4d8fe00
신지원 <s420204c@gamil.com>
2024-09-05 02:11:53 +0900

    style: 헤더컴포넌트 반응형과 기본 스타일 추가

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,22 +1,31 @@
 import Link from 'next/link';
 import Logo from '@/assets/logo.svg';
 import User from '@/assets/user.svg';
+import Menu from '@/assets/menu.svg';
 
 const Header = () => {
   return (
-    <div className="flex">
-      <div>
-        <Link href="/">
-          <Logo />
-        </Link>
+    <div className="bg-background-secondary flex justify-between items-center px-[24px] lg:px-[350px]">
+      <div className="my-[14px] flex items-center gap-[40px]">
+        <div className="flex items-center gap-[16px]">
+          <Menu className="md:hidden" />
+          <Link href="/">
+            <Logo />
+          </Link>
+        </div>
+        <ul>
+          <Link href="/boards">
+            <li className="hidden md:block text-lg text-text-primary font-medium">
+              자유게시판
+            </li>
+          </Link>
+        </ul>
       </div>
-      <ul>
-        <Link href="/boards">
-          <li>자유게시판</li>
-        </Link>
-      </ul>
-      <div>
-        <User />
+      <div className="flex items-center gap-[8px]">
+        <User className="md:w-[16px] md:h-[16px]" />
+        <p className="hidden lg:block text-text-primary text-md font-medium">
+          안해나
+        </p>
       </div>
     </div>
   );
