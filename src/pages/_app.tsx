c35bd0609117de0c473fbe48f831881a442921c0
신지원 <s420204c@gamil.com>
2024-09-06 15:15:50 +0900

    feat: 헤더 컴포넌트 조건부 렌더링 추가 및 api 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Header from '@/components/header/Header';
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </QueryClientProvider>
   );
 }
