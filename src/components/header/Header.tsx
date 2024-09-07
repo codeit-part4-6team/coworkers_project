d95825cdf85342e0ab14d744ce6da348db67f2fd
신지원 <s420204c@gamil.com>
2024-09-08 00:14:35 +0900

    feat: 헤더 컴포넌트 사이드 바 및 팀 드롭다운 추가

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,16 +5,23 @@ import Menu from '@/assets/menu.svg';
 import { useQuery } from '@tanstack/react-query';
 import basicApi from '@/lib/basicAxios';
 import { useState, useEffect } from 'react';
+import SideMenu from './SideMenu';
+import Dropdown, { DropdownOption } from '@/components/dropdown/Dropdown';
+import { Group } from '@/type/usergroup';
 
-const fetchUserData = async () => {
+const getAuthToken = () => {
+  const token = localStorage.getItem('accessToken');
+  if (!token) {
+    throw new Error('토큰이 없습니다');
+  }
+  return token;
+};
+
+const fetchData = async (endpoint: string) => {
   try {
-    const token = localStorage.getItem('accessToken');
+    const token = getAuthToken();
 
-    if (!token) {
-      throw new Error('토큰이 없습니다');
-    }
-
-    const response = await basicApi.get('/user', {
+    const response = await basicApi.get(endpoint, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
@@ -22,13 +29,25 @@ const fetchUserData = async () => {
 
     return response.data;
   } catch (error) {
-    console.error('유저 데이터 불러오기 실패', error);
+    console.error(`${endpoint} 데이터 불러오기 실패`, error);
     return null;
   }
 };
 
+const fetchUserGroups = async () => {
+  const data = await fetchData('/user/groups');
+  return data || [];
+};
+
+const fetchUserData = async () => {
+  const data = await fetchData('/user');
+  return data || null;
+};
+
 const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [isSideMenu, setIsSideMenu] = useState<boolean>(false);
+  const [selectedTeam, setSelectedTeam] = useState<DropdownOption | null>(null);
 
   useEffect(() => {
     const token = localStorage.getItem('accessToken');
@@ -42,19 +61,52 @@ const Header = () => {
     retry: false,
   });
 
+  const { data: userGroups = [] } = useQuery<Group[]>({
+    queryKey: ['userGroups'],
+    queryFn: fetchUserGroups,
+    enabled: isLoggedIn,
+    retry: false,
+  });
+
   const isLogin = !!userData || isLoggedIn;
 
+  const handleMenuClick = () => {
+    setIsSideMenu((prev) => !prev);
+  };
+
+  const handleTeamChange = (selectedOption: DropdownOption) => {
+    setSelectedTeam(selectedOption);
+  };
+
+  const teamOptions: DropdownOption[] = userGroups.map((group: Group) => ({
+    label: group.name,
+    value: group.id.toString(),
+  }));
+
+  const placeholderText = selectedTeam
+    ? selectedTeam.label
+    : teamOptions[0]?.label;
+
   return (
-    <div className="bg-background-secondary flex justify-between items-center px-[24px] lg:px-[350px]">
+    <div className="bg-background-secondary flex justify-between items-center px-[24px] lg:px-[350px] border-border-primary-10">
       <div className="my-[14px] flex items-center gap-[40px]">
         <div className="flex items-center gap-[16px]">
-          {isLogin && <Menu className="md:hidden" />}
+          {isLogin && <Menu className="md:hidden" onClick={handleMenuClick} />}
           <Link href="/">
             <Logo />
           </Link>
         </div>
         {isLogin && (
-          <ul>
+          <ul className="flex items-center gap-[40px]">
+            {teamOptions.length > 0 && (
+              <Dropdown
+                options={teamOptions}
+                onChange={handleTeamChange}
+                placeholder={placeholderText}
+                size="md"
+                className="hidden md:block"
+              />
+            )}
             <Link href="/boards">
               <li className="hidden md:block text-lg text-text-primary font-medium">
                 자유게시판
@@ -70,6 +122,15 @@ const Header = () => {
             {userData?.nickname}
           </p>
         </div>
+      )}
+      {isSideMenu && (
+        <>
+          <div
+            className="fixed inset-0 bg-blend-darken bg-black bg-opacity-50"
+            onClick={() => setIsSideMenu(false)}
+          ></div>
+          <SideMenu onClose={() => setIsSideMenu(false)} groups={userGroups} />
+        </>
       )}
     </div>
   );
--- src/components/header/SideMenu.tsx
+++ src/components/header/SideMenu.tsx
@@ -1,5 +1,30 @@
-const SideMenu = () => {
-  return <div>sidemenu</div>;
+import Cancel from '@/assets/x_icon.svg';
+import Link from 'next/link';
+import { Group } from '@/type/usergroup';
+
+interface SideMenuProps {
+  onClose: () => void;
+  groups: Group[];
+}
+
+const SideMenu = ({ onClose, groups }: SideMenuProps) => {
+  return (
+    <div className="fixed top-0 left-0 w-1/2 h-full bg-background-secondary p-[16px] border-border-primary-10 z-2">
+      <div className="flex justify-end">
+        <Cancel onClick={onClose} />
+      </div>
+      <ul className="flex flex-col gap-[24px] mt-[35px]">
+        {groups.map((group) => (
+          <li key={group.id} className="text-text-primary text-md font-medium">
+            {group.name}
+          </li>
+        ))}
+        <Link href="/boards">
+          <li className="text-text-primary text-md font-medium">자유게시판</li>
+        </Link>
+      </ul>
+    </div>
+  );
 };
 
 export default SideMenu;
