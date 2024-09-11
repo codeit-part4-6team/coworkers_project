137c8c5b5f28eb90aa9bf33df6bd7f8884624e43
Dangchan Kim <kdc5847@naver.com>
2024-09-11 17:10:53 +0900

    feat: 리스트 페이지 레이아웃 및 스타일 구현

---
+++ src/components/list/CommentWriting.tsx
@@ -0,0 +1,32 @@
+import { useRef } from 'react';
+import EnterIcon from '@/assets/enter.svg';
+import EnterActiveIcon from '@/assets/enter_active.svg';
+
+export default function CommentWriting() {
+  const commentRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleResizeHeight = () => {
+    if (commentRef.current) {
+      commentRef.current.style.height = 'auto';
+      commentRef.current.style.height = commentRef.current.scrollHeight + 'px';
+    }
+  };
+  return (
+    <div className="flex justify-between items-start mb-6 py-[13px] border-y-[1px] border-border-primary-10">
+      <label htmlFor="comment" className="hidden">
+        comment
+      </label>
+      <textarea
+        ref={commentRef}
+        rows={1}
+        placeholder="댓글을 달아주세요"
+        onChange={handleResizeHeight}
+        className="w-full h-6 pt-[3px] text-md font-regular bg-background-secondary overflow-y-hidden outline-none resize-none placeholder:text-md placeholder:font-regular placeholder:text-text-default"
+      />
+      <button type="button">
+        <EnterIcon />
+        {/* <EnterActiveIcon /> */}
+      </button>
+    </div>
+  );
+}
--- src/components/list/WorkToDoDetail.tsx
+++ src/components/list/WorkToDoDetail.tsx
@@ -1,12 +1,10 @@
-import { useRef } from 'react';
-import XIcon from '@/assets/x.svg';
+import XIcon from '@/assets/x_icon.svg';
 import KebabIcon from '@/assets/kebab_large.svg';
 import ProfileMemberIcon from '@/assets/profile_member_large.svg';
-import EnterIcon from '@/assets/enter.svg';
-import EnterActiveIcon from '@/assets/enter_active.svg';
 import CalendarNRepeat from './CalendarNRepeat';
+import CommentWriting from './CommentWriting';
+import Comment from './Comment';
 import Dropdown, { DropdownOption } from '@/components/common/Dropdown';
-import Button from '@/components/common/Button';
 import FloatingButton from '@/components/common/FloatingButton';
 
 const WorkToDoOptions: DropdownOption[] = [
@@ -15,15 +13,6 @@ const WorkToDoOptions: DropdownOption[] = [
 ];
 
 export default function WorkToDoDetail() {
-  const commentRef = useRef<HTMLTextAreaElement>(null);
-
-  const handleResizeHeight = () => {
-    if (commentRef.current) {
-      commentRef.current.style.height = 'auto';
-      commentRef.current.style.height = commentRef.current.scrollHeight + 'px';
-    }
-  };
-
   const handleWorkToDoOptionChange = (option: DropdownOption) => {
     console.log('dasd');
   };
@@ -56,67 +45,12 @@ export default function WorkToDoDetail() {
       <div className="mb-6">
         <CalendarNRepeat />
       </div>
-      <p className="mb-[100px] md:mb-[182px]">
+      <p className="mb-[100px] md:mb-[182px] text-md font-regular">
         필수 정보 10분 입력하면 3일 안에 법인 설립이 완료되는 법인 설립 서비스의
         장점에 대해 상세하게 설명드리기
       </p>
-      <div className="flex justify-between items-start mb-6 py-[13px] border-y-[1px] border-border-primary-10">
-        <label htmlFor="comment" className="hidden">
-          comment
-        </label>
-        <textarea
-          ref={commentRef}
-          rows={1}
-          placeholder="댓글을 달아주세요"
-          onChange={handleResizeHeight}
-          className="w-full h-6 pt-[3px] text-md font-regular bg-background-secondary overflow-y-hidden outline-none resize-none placeholder:text-md placeholder:font-regular placeholder:text-text-default"
-        />
-        <button type="button">
-          <EnterIcon />
-          {/* <EnterActiveIcon /> */}
-        </button>
-      </div>
-      <div className="flex flex-col gap-4 pb-4 border-b-[1px] border-border-primary-10">
-        <div className="flex justify-between items-start">
-          <p className="text-md font-regular">
-            법인 설립 서비스 관련 링크 첨부 드려요 https://www.codeit.kr
-            아아아고고고라라라니니니 링크 첨부 부탇 아아아고고고라라라니니니
-            링크 첨부 부탇 아아아고고고라라라니니니 링크 첨부
-            부탇아아고고고라라라니니니 링크 첨부 부탇 아아아고고고라라라니니니
-            링크 첨부 부탇아아고고고라라라니니니 링크 첨부 부탇
-            아아아고고고라라라니니니 링크 첨부 부탇
-          </p>
-          <Dropdown
-            options={WorkToDoOptions}
-            customButton={<KebabIcon />}
-            onChange={handleWorkToDoOptionChange}
-            size="md"
-          />
-        </div>
-        <div className="flex justify-between items-center">
-          <div className="flex items-center gap-3">
-            <ProfileMemberIcon />
-            <span className="text-md font-medium">우지은</span>
-          </div>
-          <span className="text-md font-regular text-text-secondary">
-            2025.05.30
-          </span>
-        </div>
-        <div className="flex justify-end items-center gap-2">
-          <button
-            type="button"
-            className="w-12 h-8 text-md font-semibold text-text-default hover:text-[rgba(100, 116, 139, 0.5)] active:text-text-secondary"
-          >
-            취소
-          </button>
-          <Button
-            option="outlined"
-            size="xsmall"
-            text="수정하기"
-            blank={true}
-          />
-        </div>
-      </div>
+      <CommentWriting />
+      <Comment />
       <div className="fixed right-4 bottom-6 md:right-6 md:bottom-5 lg:right-10 lg:bottom-10">
         <FloatingButton option="success" text="완료하기" disabled={false} />
         {/* <FloatingButton option="cancel" text="완료 취소하기" disabled={false} /> */}
