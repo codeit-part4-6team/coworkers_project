ecf1fa3bb4bd71948c309eb9f89ada341ecfbeb5
Dangchan Kim <kdc5847@naver.com>
2024-09-23 18:28:51 +0900

    feat: api 리스폰스로 받은 id 값 저장 zustand store 생성

---
+++ src/store/apiResponseIdsStore.ts
@@ -0,0 +1,23 @@
+import { create } from 'zustand';
+
+interface Store {
+  groupId: number;
+  setGroupId: (id: number) => void;
+  taskListId: number;
+  setTaskListId: (id: number) => void;
+  taskId: number;
+  setTaskId: (id: number) => void;
+  recurringId: number;
+  setRecurringId: (id: number) => void;
+}
+
+export const useApiResponseIdsStore = create<Store>((set) => ({
+  groupId: 0,
+  setGroupId: (id) => set({ groupId: id }),
+  taskListId: 0,
+  setTaskListId: (id) => set({ taskListId: id }),
+  taskId: 0,
+  setTaskId: (id) => set({ taskId: id }),
+  recurringId: 0,
+  setRecurringId: (id) => set({ recurringId: id }),
+}));
