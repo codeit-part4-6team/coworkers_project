65b3b4b8f2dc31df59ece3e120b0ad04dd461766
ddol9 <drddol9@gmail.com>
2024-09-28 15:35:11 +0900

    fix: delete token value in axios

--- src/lib/basicAxios.ts
+++ src/lib/basicAxios.ts
@@ -20,7 +20,7 @@ export const basicAuthAxios: AxiosInstance = axios.create({
 basicAuthAxios.interceptors.request.use(
   (config) => {
     if (typeof window !== 'undefined') {
-      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzA3LCJ0ZWFtSWQiOiI3LTYiLCJzY29wZSI6ImFjY2VzcyIsImlhdCI6MTcyNzI0MjAwOCwiZXhwIjoxNzI3MjQ1NjA4LCJpc3MiOiJzcC1jb3dvcmtlcnMifQ.bVeKClv1lY6X1BiriC-DfsiUDMGFoMmujWsnD8Ejr-E'
+      const token = localStorage.getItem('accessToken');
       if (token) {
         config.headers['Authorization'] = `Bearer ${token}`;
       }
