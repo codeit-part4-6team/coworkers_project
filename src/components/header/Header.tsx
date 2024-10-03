d4fdaaf75643c731baaedb13dffda4f9a7ea5c17
신지원 <s420204c@gamil.com>
2024-10-03 17:47:23 +0900

    style: 헤더 컴포넌트 반응형 수정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -99,109 +99,113 @@ const Header = () => {
   ];
 
   return (
-    <div className="bg-background-secondary h-[60px] flex justify-between items-center px-6 lg:px-[350px] border-b border-border-primary-10">
-      <div className="my-[14px] flex items-center gap-10">
-        <div className="flex items-center gap-4">
-          {isLogin && <Menu className="md:hidden" onClick={handleMenuClick} />}
-          <Link href="/">
-            <Logo className="lg:w-[158px] lg:h-8" />
-          </Link>
+    <div className="bg-background-secondary h-[60px] px-6 border-b border-border-primary-10">
+      <div className="flex justify-between items-center h-full 2lg:max-w-[1200px] 2lg:mx-auto">
+        <div className="my-[14px] flex items-center gap-10">
+          <div className="flex items-center gap-4">
+            {isLogin && (
+              <Menu className="md:hidden" onClick={handleMenuClick} />
+            )}
+            <Link href="/">
+              <Logo className="lg:w-[158px] lg:h-8" />
+            </Link>
+          </div>
+          {isLogin && (
+            <ul className="flex items-center gap-10">
+              {teamOptions.length > 0 && (
+                <div className="hidden md:block">
+                  <TeamDropdown
+                    options={teamOptions}
+                    onChange={handleTeamChange}
+                  />
+                </div>
+              )}
+              <li className="hidden md:block text-lg text-text-primary font-medium">
+                <Link href="/boards">자유게시판</Link>
+              </li>
+            </ul>
+          )}
         </div>
         {isLogin && (
-          <ul className="flex items-center gap-10">
-            {teamOptions.length > 0 && (
-              <div className="hidden md:block">
-                <TeamDropdown
-                  options={teamOptions}
-                  onChange={handleTeamChange}
-                />
-              </div>
-            )}
-            <li className="hidden md:block text-lg text-text-primary font-medium">
-              <Link href="/boards">자유게시판</Link>
-            </li>
-          </ul>
-        )}
-      </div>
-      {isLogin && (
-        <div className="w-[135px] flex justify-end">
-          <Dropdown
-            options={userOptions}
-            onChange={(option) => {
-              if (option.value === 'logout') {
-                localStorage.removeItem('accessToken');
-                setIsLoggedIn(false);
+          <div className="w-[135px] flex justify-end">
+            <Dropdown
+              options={userOptions}
+              onChange={(option) => {
+                if (option.value === 'logout') {
+                  localStorage.removeItem('accessToken');
+                  setIsLoggedIn(false);
+                }
+              }}
+              customButton={
+                <div className="flex justify-end gap-2 items-center cursor-pointer">
+                  <User className="md:w-4 md:h-4" />
+                  <p className="hidden lg:block text-text-primary text-md font-medium">
+                    {userData?.nickname}
+                  </p>
+                </div>
               }
-            }}
-            customButton={
-              <div className="flex justify-end gap-2 items-center cursor-pointer">
-                <User className="md:w-4 md:h-4" />
-                <p className="hidden lg:block text-text-primary text-md font-medium">
-                  {userData?.nickname}
-                </p>
-              </div>
-            }
-            size="sm"
-          />
-        </div>
-      )}
-
-      <Modal id="sideMenu" className="fixed inset-0">
-        <div className="fixed top-0 left-0 w-1/2 h-full bg-background-secondary p-4 z-50">
-          <div className="flex justify-end">
-            <Cancel onClick={() => closeModal('sideMenu')} />
+              size="sm"
+            />
           </div>
-          <ul className="flex flex-col gap-6 mt-[35px]">
-            {userGroups.map((group) => (
+        )}
+
+        <Modal id="sideMenu" className="fixed inset-0">
+          <div className="fixed top-0 left-0 w-1/2 h-full bg-background-secondary p-4 z-50">
+            <div className="flex justify-end">
+              <Cancel onClick={() => closeModal('sideMenu')} />
+            </div>
+            <ul className="flex flex-col gap-6 mt-[35px]">
+              {userGroups.map((group) => (
+                <li
+                  key={group.id}
+                  onClick={() => handleItemClick(String(group.id))}
+                  className={`text-md font-medium cursor-pointer ${
+                    selectedItem === String(group.id)
+                      ? 'text-color-brand-primary'
+                      : 'text-text-primary'
+                  }`}
+                >
+                  <Link
+                    href={`/group/${group.id}`}
+                    className="focus-visible:outline-none focus:outline-none"
+                  >
+                    {group.name}
+                  </Link>
+                </li>
+              ))}
               <li
-                key={group.id}
-                onClick={() => handleItemClick(String(group.id))}
-                className={`text-md font-medium cursor-pointer ${
-                  selectedItem === String(group.id)
+                onClick={() => handleItemClick('boards')}
+                className={`text-md font-medium cursor-pointer focus-visible:outline-none ${
+                  selectedItem === 'boards'
                     ? 'text-color-brand-primary'
                     : 'text-text-primary'
                 }`}
               >
                 <Link
-                  href={`/group/${group.id}`}
+                  href="/boards"
                   className="focus-visible:outline-none focus:outline-none"
                 >
-                  {group.name}
+                  자유게시판
                 </Link>
               </li>
-            ))}
-            <li
-              onClick={() => handleItemClick('boards')}
-              className={`text-md font-medium cursor-pointer focus-visible:outline-none ${
-                selectedItem === 'boards'
-                  ? 'text-color-brand-primary'
-                  : 'text-text-primary'
-              }`}
-            >
-              <Link
-                href="/boards"
-                className="focus-visible:outline-none focus:outline-none"
+              <li
+                onClick={() => handleItemClick('addteam')}
+                className={`text-md font-medium cursor-pointer ${
+                  selectedItem === 'addteam'
+                    ? 'text-color-brand-primary'
+                    : 'text-text-primary'
+                }`}
               >
-                자유게시판
-              </Link>
-            </li>
-            <li
-              onClick={() => handleItemClick('addteam')}
-              className={`text-md font-medium cursor-pointer ${
-                selectedItem === 'addteam'
-                  ? 'text-color-brand-primary'
-                  : 'text-text-primary'
-              }`}
-            >
-              <Link href="/addteam">팀 추가하기</Link>
-            </li>
-          </ul>
-        </div>
-        <div
-          className="fixed inset-0 bg-black bg-opacity-50"
-          onClick={() => closeModal('sideMenu')}
-        ></div>
-      </Modal>
+                <Link href="/addteam">팀 추가하기</Link>
+              </li>
+            </ul>
+          </div>
+          <div
+            className="fixed inset-0 bg-black bg-opacity-50"
+            onClick={() => closeModal('sideMenu')}
+          ></div>
+        </Modal>
+      </div>
     </div>
   );
 };
