22526e3712f03017201a81e48d9ae7d63dc7400c
ddol9 <drddol9@gmail.com>
2024-10-03 15:36:50 +0900

    refactor: taskListCard 컴포넌트 분리

---
+++ src/components/groupPage/TaskListCard/ProgressChart.tsx
@@ -0,0 +1,40 @@
+const ProgressChart = ({
+  progress,
+  size = 12,
+  strokeWidth = 2,
+}: {
+  progress: number;
+  size?: number;
+  strokeWidth?: number;
+}) => {
+  const radius = (size - strokeWidth) / 2;
+  const circumference = 2 * Math.PI * radius;
+  const offset = circumference - (progress / 100) * circumference;
+
+  return (
+    <svg width={size} height={size} className="transform -rotate-90">
+      <circle
+        cx={size / 2}
+        cy={size / 2}
+        r={radius}
+        strokeWidth={strokeWidth}
+        fill="white"
+        className="text-background-tertiary"
+        stroke="currentColor"
+      />
+      <circle
+        cx={size / 2}
+        cy={size / 2}
+        r={radius}
+        strokeWidth={strokeWidth}
+        fill="transparent"
+        strokeDasharray={circumference}
+        strokeDashoffset={offset}
+        strokeLinecap="round"
+        className="stroke-color-brand-primary"
+      />
+    </svg>
+  );
+};
+
+export default ProgressChart;
